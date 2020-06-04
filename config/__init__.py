a9858dd4998192abd9e4c6f7eaad7cd0a14a3e3f
Georgiana Lohan <gml15@outlook.es>
2020-06-04 18:28:21 +0200

    commit

---
+++ config/__init__.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+"""
+CONFIG
+En la carpeta config iran las configuraciones del juego, eso es
+configuraciones de la ventana, titulos, argumentos, etc.
+
+Al importar config se adquieren las direcciones de
+todos los directorios dentro de config/
+
+Game template
+Autor: GEORGIANA M. LOHAN
+Fecha: Marzo 2020
+"""
+
+# Importación de librerías
+from __future__ import print_function
+import os
+import sys
+
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+elif __file__:
+    application_path = os.path.dirname(__file__)
+
+# Definición de directorios
+# noinspection PyUnboundLocalVariable
+__actualpath = application_path.replace("\\", "/") + "/"
+DIR_CONFIG = __actualpath
+# DIR_CONFIG = __actualpath + 'config/'  # compilacion
+
+# Test
+if __name__ == '__main__':
+    print(DIR_CONFIG)
